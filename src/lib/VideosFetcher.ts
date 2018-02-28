a92fda05111a8dde293e52862871b87fa3618ffc
Igor Dobryn <IgorDobryn@gmail.com>
2018-02-28 17:26:24 +0200

    Display videos

--- src/lib/VideosFetcher.ts
+++ src/lib/VideosFetcher.ts
@@ -1,15 +1,16 @@
 import { XMLHttpRequest } from 'xmlhttprequest';
 import * as fs from 'fs';
+import Video from "../models/Video";
 
 class VideosFetcher {
   static url = 'https://api.contents.watchabc.go.com/vp2/ws/s/contents/3000/videos/001/001/sf/-1/-1/-1/-1/50.json';
   static testMode = true
 
-  static fetch(opts) {
+  static fetch(callback) {
     if (this.testMode) {
       const json = JSON.parse(fs.readFileSync('./spec/fixtures/sampleVideosResponse.json', 'utf8'));
 
-      opts.onLoad(this.convertJSONToVideos(json));
+      callback(this.convertJSONToVideos(json));
     } else {
       var request = new XMLHttpRequest();
       request.open('GET', this.url, true);
@@ -17,7 +18,7 @@ class VideosFetcher {
       request.onload = () => {
         const json = JSON.parse(request.responseText);
 
-        opts.onLoad(this.convertJSONToVideos(json));
+        callback(this.convertJSONToVideos(json));
       };
       request.onerror = opts.onError;
 
@@ -31,8 +32,11 @@ class VideosFetcher {
       for (var i = 0; i < json['video'].length; i += 1) {
         let video = json['video'][i];
 
-
-
+        videos.push(new Video({
+          thumbnail: json['video'][i]['thumbnails']['thumbnail'][0]['value'],
+          description: video['description'],
+          title: video['title'],
+        }))
       }
     }
 
